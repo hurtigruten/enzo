ed234e41334fe5ed71b7800795c7ceca61e29f13
Tomas Zijdemans <Tomas.Zijdemans@hurtigruten.com>
2020-04-28 18:10:26 +0200

    Forgot to add new files

---
+++ ConfigParser.ts
@@ -0,0 +1,84 @@
+import cacheConfigDev from "./samples/sampleCacheConfig.json";
+import cacheConfig from "./cacheConfig.json";
+import { buildXMLBody } from "./xmlSerializer.ts";
+
+interface cacheConfig {
+  defaultMarkets: string[],
+  defaultDaysAhead: number,
+  searchDayRange: number,
+  sailings: sailing[]
+}
+interface sailing {
+  voyageType: string,
+  direction: string,
+  daysAhead?: number,
+  fromPort: string,
+  toPort: string,
+  partyMixes: string[],
+  marketFilter?: string[]
+}
+export interface JsonSearch {
+  fromDay: string,
+  toDay: string,
+  voyageType: string,
+  voyageCode: string,
+  party: string,
+  market: string,
+}
+
+export class ConfigParser {
+  private _config: cacheConfig;
+  
+  constructor (devMode?: boolean) {
+    this._config = cacheConfigDev;
+  }
+
+  parseConfig(): string[] {
+    const searches: JsonSearch[] = produceJsonSearches(this._config);
+    return searches.map((search: JsonSearch) => buildXMLBody(search))
+  }
+}
+
+function produceJsonSearches(config: cacheConfig): JsonSearch[] {
+
+    // Flatten out partymixes, and mixin markets
+  let flatParty = config.sailings.flatMap((sailing) => {
+    return sailing.partyMixes.map((party: string) => ({
+      voyageType: sailing.voyageType,
+      daysAhead: sailing.daysAhead || config.defaultDaysAhead,
+      voyageCode: sailing.fromPort + "-" + sailing.toPort,
+      party,
+      markets: sailing.marketFilter || config.defaultMarkets
+    }));
+  });
+
+  // Flatten out markets and mixin pages
+  let flatMarket = flatParty.flatMap((sailing) => {
+    return sailing.markets.map((market: string) => ({
+      voyageType: sailing.voyageType,
+      daysAhead: sailing.daysAhead,
+      voyageCode: sailing.voyageCode,
+      market,
+      party: sailing.party,
+      pages : Array.from(Array(Math.ceil(sailing.daysAhead / config.searchDayRange)).keys()),
+     }));
+  });
+
+  // Flatten out pages and transform to date
+  return flatMarket.flatMap((sailing) => {
+    return sailing.pages.map((page: number) => ({
+      fromDay: dateFromToday(page * config.searchDayRange),
+      toDay: dateFromToday(Math.min(page * config.searchDayRange + config.searchDayRange - 1, sailing.daysAhead - 1)),
+      voyageType: sailing.voyageType,
+      voyageCode: sailing.voyageCode,
+      party: sailing.party,
+      market: sailing.market,
+    }));
+  });
+}
+
+function dateFromToday(daysAhead: number) {
+  let date = new Date();
+  date.setDate(date.getDate() + daysAhead)
+  return date.toISOString().split("T")[0];
+}
--- mod.ts
+++ mod.ts
@@ -3,6 +3,6 @@ import { ConfigParser } from './ConfigParser.ts';
 
 const configParser = new ConfigParser();
 const payload = configParser.parseConfig();
-
 const cacheLoader = new CacheLoader(payload, true);
+
 await cacheLoader.load()
