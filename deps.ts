c5f307143975458ef97ed468c6b143702a2c584d
Tomas Zijdemans <Tomas.Zijdemans@hurtigruten.com>
2021-02-23 13:20:29 +0100

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { parse } from "https://deno.land/std@0.84.0/flags/mod.ts";
-export { hasOwnProperty } from "https://deno.land/std@0.84.0/_util/has_own_property.ts";
-export { v4 } from "https://deno.land/std@0.84.0/uuid/mod.ts"
-export { Application, Context } from "https://deno.land/x/abc@v1.2.4/mod.ts";
-import * as log from "https://deno.land/std@0.84.0/log/mod.ts";
+export { parse } from "https://deno.land/std@0.88.0/flags/mod.ts";
+export { hasOwnProperty } from "https://deno.land/std@0.88.0/_util/has_own_property.ts";
+export { v4 } from "https://deno.land/std@0.88.0/uuid/mod.ts"
+export { Application, Context } from "https://deno.land/x/abc@v1.2.6/mod.ts";
+import * as log from "https://deno.land/std@0.88.0/log/mod.ts";
 export { log };
