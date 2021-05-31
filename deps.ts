596ea73fb02724dba073d49d9328e81957eda7b9
Tomas Zijdemans <tomzij@pop-os.localdomain>
2021-05-31 17:06:00 +0200

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { parse } from "https://deno.land/std@0.93.0/flags/mod.ts";
-export { hasOwnProperty } from "https://deno.land/std@0.93.0/_util/has_own_property.ts";
-export { v4 } from "https://deno.land/std@0.93.0/uuid/mod.ts"
+export { parse } from "https://deno.land/std@0.97.0/flags/mod.ts";
+export { hasOwnProperty } from "https://deno.land/std@0.97.0/_util/has_own_property.ts";
+export { v4 } from "https://deno.land/std@0.97.0/uuid/mod.ts"
 export { Application, Context } from "https://deno.land/x/abc@v1.3.1/mod.ts";
-import * as log from "https://deno.land/std@0.93.0/log/mod.ts";
+import * as log from "https://deno.land/std@0.97.0/log/mod.ts";
 export { log };
