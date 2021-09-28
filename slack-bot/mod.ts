cea5aaf0818a4550caca858825789a9cda5ef5ab
Tomas Zijdemans <tomas.zijdemans@hurtigruten.com>
2021-09-28 22:32:06 +0200

    Added experimental Slack websocket for initializing cache of single sailing

--- slack-bot/mod.ts
+++ slack-bot/mod.ts
@@ -1,3 +1,59 @@
+import { SocketModeClient } from "https://deno.land/x/slack_socket_mode@1.0.3/mod.ts"
+import { produceJsonSearches } from "../sailingsParser.ts";
+import { asyncPool } from "../seawareLoader.ts";
+import { createSeawareRequest } from "../serializeXML.ts";
+import { CacheConfig, Sailing, SailingSearch } from "../types.ts";
+
+const appToken = "xapp-1-A02FHFZ81N3-2555422715841-07eb35776dab285f5326c0fc91eff01f117dac77bb96323acf5f4d7868f34c56"
+const socketModeClient = new SocketModeClient({ appToken })
+
+// Attach listeners to events by type. See: https://api.slack.com/events/message
+socketModeClient.addEventListener("app_mention", ({ detail: { body, ack } }) => {
+    ack()
+    const str = body.event.text.split("> ")[1] || "";
+    const words = str.split(" ")
+    const fromPort = words[2];
+    const toPort = words[4];
+    if (fromPort && fromPort.length === 3 && isUpper(fromPort) && toPort && toPort.length === 3 && isUpper(toPort)) {
+        cacheSailing(fromPort, toPort);
+    }
+
+})
+
+await socketModeClient.start();
+
+function isUpper(str: string) {
+    return !/[a-z]/.test(str) && /[A-Z]/.test(str);
+}
+
+async function cacheSailing(fromPort: string, toPort:string) {
+    // Config
+    const url = "http://localhost:8085/SwBizLogic/Service.svc/ProcessRequest";
+    const POOL_SIZE = 15;
+    const configFile = "../configs/fullCache.json"
+
+    // Parse the full json config file
+    const config: CacheConfig = JSON.parse(Deno.readTextFileSync(configFile)) as CacheConfig;
+
+    // Find requested sailing in config
+    const filteredSailings: Sailing[] = config.sailings.filter(function (sailing) {
+        return sailing.fromPort === fromPort && sailing.toPort === toPort;
+    })
+
+    if (filteredSailings.length === 0) {
+        postSlackMessage(`Could not find a sailing in fullCache.json for: From port ${fromPort} to port ${toPort}`);
+    }
+
+    // Parse the config, using only the requested sailing
+    config.sailings = filteredSailings;
+    const searches: SailingSearch[] = produceJsonSearches(config);
+    const payload: string[] = searches.map((search: SailingSearch) => createSeawareRequest(search));
+
+    await asyncPool(url, POOL_SIZE, payload);
+
+    postSlackMessage(`Cached ${fromPort} - ${toPort}`);
+}
+
 
 // Post a HTTP requests to Slack
 export async function postSlackMessage(body: string): Promise<void> {
