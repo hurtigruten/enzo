f28a3019a27360ffff892688f1f5510a5024ea0f
Tomas Zijdemans <tomas.zijdemans@hurtigruten.com>
2021-10-17 18:36:44 +0200

    Remove sailings route from API

--- api/api.ts
+++ api/api.ts
@@ -1,16 +1,3 @@
 import { listenAndServe } from "../deps.ts";
-import { CacheConfig } from "../types.ts";
 
-const config: CacheConfig = JSON.parse(Deno.readTextFileSync("../configs/fullCache.json"));
-
-listenAndServe(":3000", (req:Request) => new Response(requestHandler(req)));
-
-function requestHandler(req: Request): string {
-    if (req.url === "http://localhost:3000/sailings") {
-      return JSON.stringify(config.sailings);
-    }
-    if (req.url === "http://localhost:3000/") { 
-      return JSON.stringify(config);
-  }
-  return "Invalid request"
-}
+listenAndServe(":3000", () => new Response(Deno.readTextFileSync("../configs/fullCache.json")));
