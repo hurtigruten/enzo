3e4f37d8b6db27ba8b120e788df3449cd32c3c61
Tomas Zijdemans <tomas.zijdemans@hurtigruten.com>
2021-11-04 08:26:14 +0100

    Upgrade deps

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { SocketModeClient } from "https://deno.land/x/slack_socket_mode@1.0.3/mod.ts";
-export { parse } from "https://deno.land/std@0.111.0/flags/mod.ts";
-export { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
+export { parse } from "https://deno.land/std@0.113.0/flags/mod.ts";
+export { listenAndServe } from "https://deno.land/std@0.113.0/http/server.ts";
