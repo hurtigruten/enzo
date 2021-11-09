0ff5c52f2baf5c3d82470c335e386b5456c3af21
Tomas Zijdemans <tomas.zijdemans@hurtigruten.com>
2021-11-09 17:34:28 +0100

    Added multiple connections. Updated deps

--- slack-bot/mod.ts
+++ slack-bot/mod.ts
@@ -1,72 +1,92 @@
 import { cacheSingleSailing, greet, whatIsCached } from "../cacheCmds.ts";
 
-async function getWebsocketUrl() {
-  const url = "https://slack.com/api/apps.connections.open";
-  try {
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded",
-        "Authorization":
-          "Bearer xapp-1-A02FHFZ81N3-2672165845639-c5f315333b06af43e2f33f1110da83c913010d3196455607f65638cbe0dfa1ac",
-      },
-    });
-    return await res.json();
-  } catch (error) {
-    console.log(error);
+type slackWssUrl = { "ok": boolean; "url": string };
+
+const SLACK_APPS_URL = "https://slack.com/api/apps.connections.open";
+const BEARER_TOKEN =
+  "xapp-1-A02FHFZ81N3-2672165845639-c5f315333b06af43e2f33f1110da83c913010d3196455607f65638cbe0dfa1ac";
+
+const sockets = new Set<WebSocket>();
+const delay = (ms: number|undefined) => new Promise(res => setTimeout(res, ms));
+
+// Create 2 connections
+createWebsocket(undefined);
+await delay(5000);
+createWebsocket(undefined);
+
+async function createWebsocket(deadSocket: WebSocket | undefined) {
+  //sockets.forEach(s => console.log("states: " + s.readyState))
+  if (deadSocket) {
+    sockets.delete(deadSocket);
   }
+
+  const wssResponse: slackWssUrl = await getWebsocketUrl();
+  //const newSocket = new WebSocket(wssResponse.url + "&debug_reconnects=true");
+  const newSocket = new WebSocket(wssResponse.url);
+  initializeWebsocket(newSocket);
+  sockets.add(newSocket);
 }
 
-const wssResponse = await getWebsocketUrl();
+function initializeWebsocket(socket: WebSocket) {
+  socket.onclose = (_) => createWebsocket(socket);
 
-if (wssResponse.ok) {
-  const wssUrl = wssResponse.url;
-  const socket = new WebSocket(wssUrl + "&debug_reconnects=true");
-
-  socket.onopen = function () {
-    console.log("console established");
-  };
+  //socket.onopen = (_) => console.log("connection established");
 
   socket.onmessage = function (event) {
     const message = JSON.parse(event.data);
 
-    if (message.type == "disconnect") {
-      console.log("Disconnect event caught");
-      handleDisconnect(message);
+    if (message.type === "disconnect") {
+      //console.log(message)
     }
-
     // Acknowledge message receiveed
     if (message.envelope_id) {
       const ack = { "envelope_id": message.envelope_id };
       socket.send(JSON.stringify(ack));
     }
-    const text = message?.payload?.event?.text;
-    // TODO: Recoginize user: const text = message?.payload?.event?.user;
-    if (text) {
-      console.log(text);
-      const words: string[] = text.split(" ");
-      const wordThree: string = words[2];
-      const wordFive: string = words[4];
 
-      if (text.includes("hi") || text.includes("hello") || text.includes("yo")) {
-        greet();
-      }
+    if (message.payload) {
+      const message = JSON.parse(event.data);
+      const text = message?.payload?.event?.text;
 
-      if (
-        wordThree && wordThree.length === 3 && wordFive && wordFive.length === 3
-      ) {
-          console.log("Caching: " + wordThree + " to " + wordFive)
-        cacheSingleSailing(wordThree, wordFive);
-      }
-      if (text.includes("cached?")) {
-        whatIsCached();
+      // TODO: Recoginize user: const text = message?.payload?.event?.user;
+      // TODO: Catch ABC instead of string parsing
+      if (text) {
+        const str: string = text.split("> ")[1] || "";
+        const words: string[] = str.split(" ");
+        const wordThree: string = words[2];
+        const wordFive: string = words[4];
+
+        if (
+          text.includes("hi") || text.includes("hello") || text.includes("yo")
+        ) {
+          greet();
+        }
+
+        if (
+          wordThree && wordThree.length === 3 && wordFive &&
+          wordFive.length === 3
+        ) {
+          console.log("Caching: " + wordThree + " to " + wordFive);
+          cacheSingleSailing(wordThree, wordFive);
+        }
+        if (text.includes("cached?")) whatIsCached();
       }
     }
   };
 }
-function handleDisconnect(message: any) {
-  // TODO
-  // warning (you don't always get it): {"type":"disconnect","reason":"warning","debug_info":{"host":"applink-canary-688c894b56-rwj4p"}}
-  // use multiple connections to handle disconnect: {"type":"disconnect","reason":"refresh_requested","debug_info":{"host":"applink-canary-688c894b56-rwj4p"}}
-  console.log(message);
+
+async function getWebsocketUrl(): Promise<slackWssUrl> {
+  try {
+    const res = await fetch(SLACK_APPS_URL, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+        "Authorization": "Bearer " + BEARER_TOKEN,
+      },
+    });
+    return await res.json();
+  } catch (e) {
+    console.log("Could not connect to Slack: " + e);
+    return { "ok": false, "url": "" };
+  }
 }
