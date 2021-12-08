6d44a00d8de9751c4df34dad12e31069af3ee9c4
Tomas Zijdemans <tomas.zijdemans@hurtigruten.com>
2021-12-08 17:02:41 +0100

    Add support for environment parameter

--- singleRun.ts
+++ singleRun.ts
@@ -1,5 +1,6 @@
 import { fullRun } from "./cacheCmds.ts";
 
-const CONFIG = "./configs/fullCache.json";
+// If the user supplies "staging" as the first parameter, use staging config
+const CONFIG = (Deno.args[0] == "staging") ? "./configs/stagingCache.json" : "./configs/fullCache.json"
 
 await fullRun(CONFIG);
