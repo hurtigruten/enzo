c157c6f3408b756bef79c172b86a7354af612822
Tomas Zijdemans <tomas.zijdemans@hurtigruten.com>
2021-12-09 13:24:01 +0100

    Add environment parameter to API

--- api/api.ts
+++ api/api.ts
@@ -1,5 +1,6 @@
 import { serve } from "../deps.ts";
 
-const CONFIG = "../configs/fullCache.json";
+// If the user supplies "staging" as the first parameter, use staging config
+const CONFIG = (Deno.args[0] == "staging") ? "../configs/stagingCache.json" : "../configs/fullCache.json"
 
 serve(() => new Response(Deno.readTextFileSync(CONFIG)), { addr: ":3000" });
