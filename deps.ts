47c40a686f314918563183f30d7a669e011b1847
Tomas Zijdemans <tomas.zijdemans@hurtigruten.com>
2022-02-25 13:24:50 +0100

    Extend BGO-BGO, KKN.BGO and BGO-KKN. Remove BGO-TOS

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { serve } from "https://deno.land/std@0.126.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.127.0/http/server.ts";
 export { config as dotEnvConfig } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
