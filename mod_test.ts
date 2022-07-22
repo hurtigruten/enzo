9a177c459f8d2cab30e1793df750a8620a713f9e
Tomas Zijdemans <tomas.zijdemans@hurtigruten.com>
2022-07-22 10:57:29 +0200

    fix: Just one more test

---
+++ mod_test.ts
@@ -0,0 +1,30 @@
+import { assertEquals } from "./deps.ts";
+import { requestRunner } from "./mod.ts";
+import { EnvironmentConfig } from "./types.ts";
+
+Deno.test("Mod - Empty runner", async () => {
+  const envConfig: EnvironmentConfig = {
+    tourAPI: "",
+    bizlogicAPI: "",
+  };
+  const runner = await requestRunner({}, envConfig);
+  assertEquals(runner, undefined);
+});
+
+Deno.test("Mod - Request at localhost ", async () => {
+  const envConfig: EnvironmentConfig = {
+    tourAPI: "",
+    bizlogicAPI: "http://localhost:3000",
+  };
+  const popOptions = {
+    tours: false,
+    tourFilter: ``,
+    voyages: true,
+    voyageFilter: ``,
+    readMode: false,
+    ignoreTourDates: true,
+    broadcastMessage: ``,
+  };
+  const runner = await requestRunner(popOptions, envConfig);
+  assertEquals(runner, undefined);
+});
