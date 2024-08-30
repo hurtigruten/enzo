0ee129674586e536f5a4e242ce274ca941a055f3
DarrenCannHRG <darren.cann@hurtigruten.com>
2024-08-30 15:22:44 +0200

    f

--- requests/pool.ts
+++ requests/pool.ts
@@ -41,7 +41,7 @@ export async function postRequest(xmlBody: string, url: string) {
     const output = response.text();
 
     var filename = "./logs/test-" + crypto.randomUUID() + ".json";
-    await Deno.writeTextFile(filename, output);
+    await Deno.writeTextFile(filename, response);
 
     return output;
   } catch (error) {
